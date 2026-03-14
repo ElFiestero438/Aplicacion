e13c9c81ffedc1371fe0a14d7e17b34f4e783d02
DanielDev87 <danielhymtj@icloud.com>
2026-03-14 09:52:26 -0500

    Actualizando App js

---
+++ src/services/firebaseService.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_CMMIl_AU7cVfkk4GcVrr5St0aBTfmtE",
+  authDomain: "proy-curso-movil-1.firebaseapp.com",
+  projectId: "proy-curso-movil-1",
+  storageBucket: "proy-curso-movil-1.firebasestorage.app",
+  messagingSenderId: "553022502322",
+  appId: "1:553022502322:web:0213f81280b79f00cca0ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
