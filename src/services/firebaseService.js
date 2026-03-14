239deda5bf39c5d6a5b7ac1d35e08ecd444690ea
DanielDev87 <168884451+DanielDev87@users.noreply.github.com>
2026-03-14 09:53:24 -0500

    Remove Firebase configuration from firebaseService.js
    Removed sensitive Firebase configuration details.

--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -5,12 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA_CMMIl_AU7cVfkk4GcVrr5St0aBTfmtE",
-  authDomain: "proy-curso-movil-1.firebaseapp.com",
-  projectId: "proy-curso-movil-1",
-  storageBucket: "proy-curso-movil-1.firebasestorage.app",
-  messagingSenderId: "553022502322",
-  appId: "1:553022502322:web:0213f81280b79f00cca0ab"
+  
 };
 
 // Initialize Firebase
